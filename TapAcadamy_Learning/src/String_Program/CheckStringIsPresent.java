27ff704cc8a2dd074bec7665f79bcb1ba9d7eb45
IT-R&D <IT-R&D@RS8021.rasiseeds.com>
2026-06-22 18:04:05 +0530

    new

--- TapAcadamy_Learning/src/String_Program/CheckStringIsPresent.java
+++ TapAcadamy_Learning/src/String_Program/CheckStringIsPresent.java
@@ -3,13 +3,14 @@ import java.util.*;
 public class CheckStringIsPresent {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		String t = sc.nextLine();
-		System.out.println(checkStringIsPresent(str, t));
+		String str = sc.nextLine();//mississippi
+		String t = sc.nextLine();//issi
+		System.out.println(checkStringIsPresent(str, t)); //Yes
 	}
 
 	public static String checkStringIsPresent(String str, String t) {
 		// TODO Auto-generated method stub
+		// all substring check the given string is present
 		int n = t.length();
 		for(int i = 0; i <= str.length() - n; i++) {
 			String temp = "";
