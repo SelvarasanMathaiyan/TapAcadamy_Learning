759ca5c37507013b0507b0321375a8be20ff9ba7
selva <selva@Selvarasan>
2026-07-09 07:35:31 +0530

    09-07-2026 add new concept.

---
+++ TapAcadamy_Learning/src/ObjectOrientedConcepts/Encapsulation.java
@@ -0,0 +1,28 @@
+package ObjectOrientedConcepts;
+import java.util.*;
+class Customer1{
+	private int cid;
+	private String cname;
+	private long cnumber;
+	public Customer1(int cid, String cname, long cnumber) { //Constructor is does not return type. It is must same name of class name.
+		this.cid = cid; //Shadowing error(name clash occur without this keyword.). so use same name for instance variable and local variable. use this keyword for instance variable.
+		this.cname = cname;
+		this.cnumber = cnumber;
+	}
+	public int getCid() {
+		return cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public long getCnumber() {
+		return cnumber;
+	}
+}
+// Encapsulation is a process of protecting important components of objects. it is preventing direct access. Encapsulation is not preventing access. It is providing controlled access.
+public class Encapsulation {
+	public static void main(String args[]) {
+		Customer1 c1 = new Customer1(101, "Selva", 701002582); //default setter in java. It is a parameterized constructors. constructor is a specialized setter in java. It is during object creation, helps initialized the object.
+		System.out.println(c1.getCid() + " " + c1.getCname() + " " + c1.getCnumber());
+	}
+}
