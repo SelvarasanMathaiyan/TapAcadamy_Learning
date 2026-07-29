d9a5ee64466c2caa6d3a45a6ff43204a4869f802
IT-R&D <IT-R&D@RS8021.rasiseeds.com>
2026-07-29 16:01:37 +0530

    added new

---
+++ TapAcadamy_Learning/src/ObjectOrientedConcepts/MethodOverridding.java
@@ -0,0 +1,44 @@
+package ObjectOrientedConcepts;
+class Testing1{
+	void fun1() { //default method
+		System.out.println("Parent Class Method");
+	}
+	void fun2() {
+		
+	}
+}
+public class MethodOverridding extends Testing1{
+//	private void fun1() {} // RULE1: Child class methods access specifiers are same and increase only accessible. Otherwise not accessible.
+//	void fun1() {};
+	public void fun1() {
+		System.out.println("Child Class Overridden Method");
+	}
+//	int fun2() { // RULE2: Child class methods return type same for parent class method. If it is changed not accessible.
+//		return 0;
+//	}
+}
+
+// RULE3: When the child class method overrides the parent class method, then the child class return the parent class object. It is called the covariant type.
+class Planes{
+	
+}
+class CargoPlanes extends Planes{
+	
+}
+
+class Testing2{
+	Planes fun1() {
+		System.out.println("Parent Class Method");
+		Planes p = new Planes();
+		return p;
+	}
+}
+
+class Testing3 extends Testing2{
+	CargoPlanes fun1() {
+		System.out.println("Child Class Overridden Method");
+		CargoPlanes cp = new CargoPlanes();
+		return cp;
+	}
+}
+
